a2ec85361f1de33b91304c78f22d76b137083b11
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-07-26 21:54:07 +0100

    Create bootloader crate

---
+++ bootloader/src/main.rs
@@ -0,0 +1,38 @@
+#![no_main]
+#![no_std]
+
+extern crate alloc;
+
+use uefi::prelude::*;
+
+#[entry]
+fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    loop {}
+}
+
+#[panic_handler]
+fn panic_handler(info: &PanicInfo) -> ! {
+    error!("{}", info);
+    loop {}
+}
+
+/*
+| UEFI type                  | Use                                                              |
+|----------------------------|------------------------------------------------------------------|
+| EfiReservedMemoryType      | Unusable memory                                                  |
+| EfiLoaderCode              | Bootloader code                                                  |
+| EfiLoaderData              | Bootloader data and memory allocations by bootloader             |
+| EfiBootServicesCode        | Boot services driver code - preserve to use boot services        |
+| EfiBootServicesData        | Boot services driver data - preserve to use boot services        |
+| EfiRuntimeServicesCode     | Runtime services driver code - preserve to use runtime services |
+| EfiRuntimeServicesData     | Runtime services driver data - preserve to use runtime services |
+| EfiConventionalMemory      | Free memory                                                      |
+| EfiUnusableMemory          | Memory with errors detected                                      |
+| EfiACPIReclaimMemory       | Memory containing ACPI tables - preserve until parsing ACPI      |
+| EfiACPIMemoryNVS           | ACPI firmware memory that must be preserved across sleep         |
+| EfiMemoryMappedIO          | ???                                                              |
+| EfiMemoryMappedIOPortSpace | ???                                                              |
+| EfiPalCode                 | ???                                                              |
+| EfiPersistentMemory        | Non-volatile but otherwise conventional memory                   |
+| EfiUnacceptedMemoryType    | ???                                                              |
+ */
