eea3db9e70e307188b7fcfaf3e3cf7cd1b53e96d
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-07-26 19:52:37 +0100

    Add serial print to kernel entry

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,11 +5,14 @@ use alloc::alloc::alloc;
 use core::alloc::{GlobalAlloc, Layout};
 use core::arch::asm;
 use log::{debug, info, warn};
+use kernel::io::serial;
 use core::fmt::Write;
 use core::panic::PanicInfo;
 use core::ptr::slice_from_raw_parts_mut;
+use kernel::io::serial::SERIAL0;
 #[export_name = "_start"]
-extern "sysv64" fn kstart(/*handoff_data: utils::handoff::Data*/) -> ! {
+	serial::init_serial0().expect("Failed to initialise serial0");
+	writeln!(SERIAL0.lock(), "Hello world!").unwrap();
 
 	//#[cfg(test)] test_main();
 	#[cfg(not(test))] kmain(/*handoff_data*/)
