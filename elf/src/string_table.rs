a930a58feb40b107795c50df1a4597daba7b601a
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-07-26 21:14:38 +0100

    Add dynamic string and symbol table getters

--- elf/src/string_table.rs
+++ elf/src/string_table.rs
@@ -2,6 +2,7 @@ use core::ffi;
 use core::ffi::CStr;
 use core::num::NonZeroU32;
 use core::ptr::slice_from_raw_parts;
+use crate::dynamic_table::DynamicTableEntry;
 
 pub struct StringTable<'a>(&'a [ffi::c_char]);
 
@@ -19,5 +20,26 @@ pub struct StringIndex(pub NonZeroU32);
 impl From<u32> for StringIndex {
 	fn from(value: u32) -> Self {
 		Self(NonZeroU32::try_from(value).unwrap())
+	}
+}
+
+impl<'a> super::File<'a> {
+	pub fn dynamic_string_table(&self) -> Option<StringTable<'_>> {
+		let string_table_addr = self.dynamic_table().find_map(|entry| match entry {
+			DynamicTableEntry::StringTable(addr) => Some(addr),
+			_ => None
+		})?;
+
+		let string_table_length = self.dynamic_table().find_map(|entry| match entry {
+			DynamicTableEntry::StringTableSize(len) => Some(len),
+			_ => None
+		})?;
+
+		let string_table = {
+			let string_table_ptr = self.data_at_address(string_table_addr).unwrap();
+			unsafe { &*slice_from_raw_parts(string_table_ptr.cast::<ffi::c_char>(), usize::try_from(string_table_length).unwrap()) }
+		};
+
+		Some(StringTable(string_table))
 	}
 }
--- elf/src/symbol_table.rs
+++ elf/src/symbol_table.rs
@@ -1,6 +1,7 @@
 use core::ffi::CStr;
 use core::ptr::slice_from_raw_parts;
 use log::debug;
+use crate::dynamic_table::DynamicTableEntry;
 use crate::{ExecutableAddressRelocated, ExecutableAddressUnrelocated};
 use super::string_table::StringIndex;
 
@@ -29,4 +30,27 @@ impl SymbolInfo {
 	pub fn is_weak(&self) -> bool { self.get_binding() == Self::WEAK }
 	fn get_binding(&self) -> u8 { self.0 & 0xf }
 	fn get_type(&self) -> u8 { self.0 >> 4 }
+}
+
+impl<'a> super::File<'a> {
+	pub fn dynamic_symbol_table(&self) -> Option<&[SymbolTableEntry]> {
+		let symbol_table_addr = self.dynamic_table().find_map(|entry| match entry {
+			DynamicTableEntry::SymbolTable(addr) => Some(addr),
+			_ => None
+		})?;
+
+		let hash_table_addr = self.dynamic_table().find_map(|entry| match entry {
+			DynamicTableEntry::HashTable(addr) => Some(addr),
+			_ => None
+		})?;
+
+		let hashtable_length = unsafe { *self.data_at_address(hash_table_addr).unwrap().cast::<u32>().offset(1) };
+		let symbol_table = {
+			let symbol_table_ptr = self.data_at_address(symbol_table_addr).unwrap();
+			unsafe { &*slice_from_raw_parts(symbol_table_ptr.cast::<SymbolTableEntry>(), usize::try_from(hashtable_length).unwrap()) }
+		};
+
+		Some(symbol_table)
+	}
+}
 }
