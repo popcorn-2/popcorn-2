484a6993eef7c68d68803248de4b1d559fc53394
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-07-26 22:38:59 +0100

    Start adding HAL for amd64

---
+++ hal/hal_amd64/build.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let link_script = "../kernel/src/arch/amd64/linker_module.ld";
+    let link_script = Path::new(link_script);
+
+    let flags = ["-C", "link-args=-export-dynamic", /*"-C", "prefer-dynamic",*/ "-Z", "export-executable-symbols=on"];
+
+    if env::var("TARGET").unwrap() == "x86_64-unknown-popcorn" {
+        println!("cargo:rustc-link-arg=-T{}", link_script.canonicalize().unwrap().display());
+    }
+}
