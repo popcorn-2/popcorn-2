aa44ca4ec2591cf25aba3e9c1d337f0f0aaeb3cc
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-07-28 14:48:44 +0100

    Add targa unit tests and dix bug in pixel iterator

--- targa/src/lib.rs
+++ targa/src/lib.rs
@@ -89,14 +89,17 @@ impl<'a, 'b> Iterator for PixelIterator<'a, 'b> {
     type Item = ((usize, usize), Pixel);
 
     fn next(&mut self) -> Option<Self::Item> {
+        if self.position.1 >= self.image.height { return None; }
+
+        let ret = Some((self.position, self.image.get_pixel(self.position.0, self.position.1)?));
+
         self.position.0 += 1;
         if self.position.0 >= self.image.width {
             self.position.1 += 1;
             self.position.0 = 0;
         }
 
-        if self.position.1 >= self.image.height { return None; }
-        Some((self.position, self.image.get_pixel(self.position.0, self.position.1)?))
+        ret
     }
 }
 
@@ -166,5 +169,106 @@ impl<'a> Image<'a> {
             color_format: format,
             ordering,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::borrow::Cow;
+    use alloc::vec;
+    use crate::{Image, Pixel, PixelFormat, PixelOrderHorizontal, PixelOrderVertical};
+
+    #[test]
+    fn returns_none_for_out_of_bounds_pixel() {
+        let test_image = Image {
+            pixel_data: Cow::Borrowed(&[]),
+            width: 0,
+            height: 0,
+            color_format: PixelFormat::Rgba8888,
+            ordering: (PixelOrderVertical::TopToBottom, PixelOrderHorizontal::LeftToRight),
+        };
+
+        assert!(test_image.get_pixel(5, 7).is_none());
+    }
+
+    #[test]
+    fn horizontal_pixel_ordering_correct() {
+        // Targa is stored as BGRA
+        const PIX_1: [u8; 4] = [0xaa, 0xbb, 0xcc, 0xff];
+        const PIX_2: [u8; 4] = [0x11, 0x22, 0x33, 0xff];
+
+        let mut test_image_data = vec![];
+        test_image_data.extend(PIX_1);
+        test_image_data.extend(PIX_2);
+
+        let test_image_ltr = Image {
+            pixel_data: Cow::Borrowed(&test_image_data),
+            width: 2,
+            height: 1,
+            color_format: PixelFormat::Rgba8888,
+            ordering: (PixelOrderVertical::TopToBottom, PixelOrderHorizontal::LeftToRight),
+        };
+
+        let test_image_rtl = Image {
+            pixel_data: Cow::Borrowed(&test_image_data),
+            width: 2,
+            height: 1,
+            color_format: PixelFormat::Rgba8888,
+            ordering: (PixelOrderVertical::TopToBottom, PixelOrderHorizontal::RightToLeft),
+        };
+
+        let ltr_0_0 = test_image_ltr.get_pixel(0, 0).unwrap();
+        assert_eq!(ltr_0_0, Pixel { r: PIX_1[2], g: PIX_1[1], b: PIX_1[0], a: PIX_1[3] });
+        let ltr_1_0 = test_image_ltr.get_pixel(1, 0).unwrap();
+        assert_eq!(ltr_1_0, Pixel { r: PIX_2[2], g: PIX_2[1], b: PIX_2[0], a: PIX_2[3] });
+
+        let rtl_0_0 = test_image_rtl.get_pixel(0, 0).unwrap();
+        assert_eq!(rtl_0_0, Pixel { r: PIX_2[2], g: PIX_2[1], b: PIX_2[0], a: PIX_2[3] });
+        let rtl_1_0 = test_image_rtl.get_pixel(1, 0).unwrap();
+        assert_eq!(rtl_1_0, Pixel { r: PIX_1[2], g: PIX_1[1], b: PIX_1[0], a: PIX_1[3] });
+    }
+
+    #[test]
+    fn decodes_rgb888_correctly() {
+        let test_image_data = vec![0xaa, 0xbb, 0xcc];
+
+        let test_image = Image {
+            pixel_data: Cow::Borrowed(&test_image_data),
+            width: 1,
+            height: 1,
+            color_format: PixelFormat::Rgb888,
+            ordering: (PixelOrderVertical::TopToBottom, PixelOrderHorizontal::LeftToRight),
+        };
+
+        let pix_0_0 = test_image.get_pixel(0, 0).unwrap();
+        assert_eq!(pix_0_0, Pixel{ r: 0xcc, g: 0xbb, b: 0xaa, a: 255 });
+    }
+
+    #[test]
+    fn pixel_iterator_yields_correct_data() {
+        let test_image_data = vec![0xaa, 0xbb, 0xcc, 0x11, 0x22, 0x33, 0xdd, 0xee, 0xff, 0x44, 0x55, 0x66];
+
+        let test_image = Image {
+            pixel_data: Cow::Borrowed(&test_image_data),
+            width: 2,
+            height: 2,
+            color_format: PixelFormat::Rgb888,
+            ordering: (PixelOrderVertical::TopToBottom, PixelOrderHorizontal::LeftToRight),
+        };
+
+        let mut iter = test_image.into_iter();
+        assert_eq!(iter.next(), Some((
+            (0, 0), Pixel{ r: 0xcc, g: 0xbb, b: 0xaa, a: 255 }
+        )));
+        assert_eq!(iter.next(), Some((
+            (1, 0), Pixel{ r: 0x33, g: 0x22, b: 0x11, a: 255 }
+        )));
+        assert_eq!(iter.next(), Some((
+            (0, 1), Pixel{ r: 0xff, g: 0xee, b: 0xdd, a: 255 }
+        )));
+        assert_eq!(iter.next(), Some((
+            (1, 1), Pixel{ r: 0x66, g: 0x55, b: 0x44, a: 255 }
+        )));
+        assert_eq!(iter.next(), None);
     }
 }
