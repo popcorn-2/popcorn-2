18d61092a45de20c9bf648b1c6e52de879ded48d
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-07-30 20:59:55 +0100

    Switch from kernel being lib+exe to just exe

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,21 +1,28 @@
+#![feature(custom_test_frameworks)]
+#![test_runner(tests::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+#![feature(result_option_inspect)]
+#![feature(const_trait_impl)]
+#![feature(pointer_byte_offsets)]
+#![feature(lang_items)]
+#![feature(allocator_api)]
 #![no_std]
 #![no_main]
 
 extern crate alloc;
 
-use alloc::alloc::alloc;
 use core::alloc::{GlobalAlloc, Layout};
-use core::arch::asm;
-use log::{debug, info, warn};
-use kernel::io::serial;
+use core::cell::{Cell, RefCell};
+use crate::io::serial;
+use crate::io::serial::SERIAL0;
 use core::fmt::Write;
 use core::panic::PanicInfo;
 use core::ptr::slice_from_raw_parts_mut;
-use kernel::io::serial::SERIAL0;
-//use kernel::memory;
-//use kernel::memory::watermark_allocator::WatermarkAllocator;
-use kernel_exports::memory::{Frame, PhysicalAddress, PhysicalMemoryAllocator};
-use utils::handoff::Range;
+use core::sync::atomic::{AtomicUsize, Ordering};
+use kernel_exports::sync::Lock;
+
+mod sync;
+mod io;
 
 #[export_name = "_start"]
 extern "sysv64" fn kstart(handoff_data: utils::handoff::Data) -> ! {
@@ -83,6 +90,67 @@ pub unsafe extern "Rust" fn __popcorn_module_alloc_zeroed(layout: Layout) -> *mu
 #[no_mangle]
 pub unsafe extern "Rust" fn __popcorn_module_realloc(ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
 	alloc::alloc::realloc(ptr, layout, new_size)
+}
+
+mod arch {
+	use core::arch::asm;
+	use core::marker::PhantomData;
+
+	pub trait PortWidth {}
+	impl PortWidth for u8 {}
+	impl PortWidth for u16 {}
+	impl PortWidth for u32 {}
+
+	#[derive(Debug, Copy, Clone)]
+	pub struct Port<T>(u16, PhantomData<T>) where T: PortWidth;
+
+	impl<T> Port<T> where T: PortWidth {
+		pub const fn new(addr: u16) -> Self {
+			Self(addr, PhantomData)
+		}
+	}
+
+	impl Port<u8> {
+		#[inline(always)]
+		pub unsafe fn read(&self) -> u8 {
+			let ret;
+			unsafe { asm!("in al, dx", in("dx") self.0, out("al") ret, options(nostack, preserves_flags)); }
+			ret
+		}
+
+		#[inline(always)]
+		pub unsafe fn write(&mut self, val: u8) {
+			unsafe { asm!("out dx, al", in("dx") self.0, in("al") val, options(nostack, preserves_flags)); }
+		}
+	}
+
+	impl Port<u16> {
+		#[inline(always)]
+		pub unsafe fn read(&self) -> u16 {
+			let ret;
+			unsafe { asm!("in ax, dx", in("dx") self.0, out("ax") ret, options(nostack, preserves_flags)); }
+			ret
+		}
+
+		#[inline(always)]
+		pub unsafe fn write(&mut self, val: u16) {
+			unsafe { asm!("out dx, ax", in("dx") self.0, in("ax") val, options(nostack, preserves_flags)); }
+		}
+	}
+
+	impl Port<u32> {
+		#[inline(always)]
+		pub unsafe fn read(&self) -> u32 {
+			let ret;
+			unsafe { asm!("in eax, dx", in("dx") self.0, out("eax") ret, options(nostack, preserves_flags)); }
+			ret
+		}
+
+		#[inline(always)]
+		pub unsafe fn write(&mut self, val: u32) {
+			unsafe { asm!("out dx, eax", in("dx") self.0, in("eax") val, options(nostack, preserves_flags)); }
+		}
+	}
 }
 
 #[global_allocator]
