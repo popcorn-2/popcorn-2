5f4813b1f34ef7df537fd4d52ad93b1fd31c229d
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-12-16 16:46:06 +0000

    Improve subcommand arg parsing

--- pop.py
+++ pop.py
@@ -1,46 +1,82 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import subprocess
 import sys
 
-result = subprocess.run([
-    "cargo",
+parser = argparse.ArgumentParser("pop.py")
+
+parser.add_argument(
+    choices=["build", "run", "test", "clean"],
+    dest="subcommand"
+)
+parser.add_argument("-v", "--verbose", action='count', default=0)
+parser.add_argument("--arch", choices=["x86_64"], required=True)
+
+args, subcommand_parse = parser.parse_known_args()
+
+match args.subcommand:
+    case "build":
+        pass
+    case "run":
+        pass
+    case "clean":
+        pass
+
+cargo_flags = []
+
+if args.verbose >= 2:
+    cargo_flags.append("--verbose")
+
+
+def run_cargo_command(subcommand: str, *cargo_args: [str]):
+    command = [
+        "cargo",
+        subcommand,
+        *cargo_flags,
+        *cargo_args
+    ]
+    if args.verbose >= 1:
+        print(" ".join(command))
+
+    return subprocess.run(command)
+
+
+result = run_cargo_command(
     "build",
     "-p", "bootloader",
-    "--target", "x86_64-unknown-uefi",
-])
+    "--target", "x86_64-unknown-uefi"
+)
 
 if result.returncode != 0:
     sys.exit("Bootloader build failed")
 
-result = subprocess.run([
-    "cargo",
+result = run_cargo_command(
     "rustc",
     "-p", "kernel",
-    "--target", "x86_64-unknown-popcorn.json",
-    "-Zbuild-std=compiler_builtins,core,alloc", "-Zbuild-std-features=compiler-builtins-mem",
-    "--",
-    "-C", "link-args=-export-dynamic",
-    "-Z", "export-executable-symbols=on",
-    "-C", "relocation-model=static",
-    "-C", "symbol-mangling-version=v0",
-    "-C", "panic=unwind",
-    "-C", "link-args=-Tkernel/src/arch/amd64/linker.ld",
-])
+   "--target", "x86_64-unknown-popcorn.json",
+   "-Zbuild-std=compiler_builtins,core,alloc", "-Zbuild-std-features=compiler-builtins-mem",
+   "--",
+   "-C", "link-args=-export-dynamic",
+   "-Z", "export-executable-symbols=on",
+   "-C", "relocation-model=static",
+   "-C", "symbol-mangling-version=v0",
+   "-C", "panic=unwind",
+   "-C", "link-args=-Tkernel/src/arch/amd64/linker.ld",
+)
 
 if result.returncode != 0:
     sys.exit("Kernel build failed")
 
-result = subprocess.run([
-    "cargo",
+result = run_cargo_command(
     "rustc",
     "-p", "popfs",
     "--bin", "popfs_uefi_driver",
     "--target", "x86_64-unknown-uefi",
     "--",
     "-Z", "pre-link-args=/subsystem:efi_boot_service_driver",
-])
+)
 
 if result.returncode != 0:
     sys.exit("popfs build failed")
@@ -49,7 +85,7 @@ result = subprocess.run([
     "cargo",
     "run",
     "-p", "builder",
-], env = {
+], env={
     **os.environ,
     "CARGO_BIN_FILE_KERNEL": "target/x86_64-unknown-popcorn/debug/kernel.exec",
     "CARGO_BIN_FILE_BOOTLOADER": "target/x86_64-unknown-uefi/debug/bootloader.efi",
