cf8dfff0f14d463fcc3141e4000a3048ed8be00f
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-12-16 17:39:03 +0000

    Add icons and fonts for bootloader

---
+++ fonts/fonts.h
@@ -0,0 +1,18 @@
+#ifndef FONTS_QUICKSAND_256_H
+#define FONTS_QUICKSAND_256_H
+
+#include "lvgl/lvgl.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern lv_font_t quicksand_200;
+extern lv_font_t open_sans_48;
+extern lv_font_t sf_symbols_48;
+extern lv_img_dsc_t cursor;
+
+#ifdef __cplusplus
+}
+#endif
+#endif // FONTS_QUICKSAND_256_H
