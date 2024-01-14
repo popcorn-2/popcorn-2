0a11eb78de8e3d8d12add4add03810e2c73bcc1f
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2024-01-14 01:09:12 +0000

    Add waker support to executor

--- kernel/src/task/executor.rs
+++ kernel/src/task/executor.rs
@@ -1,35 +1,77 @@
 use alloc::boxed::Box;
-use alloc::collections::VecDeque;
+use alloc::collections::BTreeMap;
+use alloc::sync::Arc;
+use alloc::task::Wake;
 use core::future::Future;
-use core::task::{Context, Poll, Waker};
+use core::task::{Context, Poll};
+use crossbeam_queue::ArrayQueue;
 use crate::task::Task;
 
 pub struct Executor {
-	tasks: VecDeque<Task>
+	next_task_id: usize,
+	pending_run: Arc<ArrayQueue<usize>>,
+	tasks: BTreeMap<usize, Task>,
+	wakers: BTreeMap<usize, core::task::Waker>
 }
 
 impl Executor {
 	pub fn new() -> Self {
 		Self {
-			tasks: VecDeque::new()
+			next_task_id: 0,
+			pending_run: Arc::new(ArrayQueue::new(128)),
+			tasks: BTreeMap::new(),
+			wakers: BTreeMap::new()
 		}
 	}
 
 	pub fn spawn<F: Future<Output = ()> + 'static>(&mut self, task: impl FnOnce() -> F) {
+		let id = self.next_task_id;
+		self.next_task_id += 1;
 		let task = Task {
-			future: Box::pin(task())
+			future: Box::pin(task()),
 		};
-		self.tasks.push_back(task);
+		self.pending_run.push(id)
+		    .expect("Unable to spawn task");
+		self.tasks.insert(id, task);
+
+		let waker = Arc::new(Waker {
+			list: self.pending_run.clone(),
+			task_to_wake: id
+		});
+		self.wakers.insert(id, waker.into());
 	}
 
-	pub fn run(&mut self) {
-		while let Some(mut task) = self.tasks.pop_front() {
-			let waker = Waker::noop();
-			let mut context = Context::from_waker(&waker);
-			match task.poll(&mut context) {
-				Poll::Ready(_) => {}
-				Poll::Pending => self.tasks.push_back(task)
+	pub fn run(&mut self) -> ! {
+		loop {
+			while let Some(id) = self.pending_run.pop() {
+				let task = self.tasks.get_mut(&id)
+				               .expect("Task does not exist");
+
+				let mut context = Context::from_waker(self.wakers.get(&id).unwrap());
+				match task.poll(&mut context) {
+					Poll::Ready(_) => {
+						self.tasks.remove(&id);
+						self.wakers.remove(&id);
+					}
+					Poll::Pending => {}
+				}
 			}
 		}
+	}
+}
+
+struct Waker {
+	list: Arc<ArrayQueue<usize>>,
+	task_to_wake: usize
+}
+
+impl Wake for Waker {
+	fn wake(self: Arc<Self>) {
+		self.wake_by_ref();
+	}
+
+	fn wake_by_ref(self: &Arc<Self>) {
+		self.list.push(self.task_to_wake)
+		    .expect("Unable to wake task")
 	}
 }
