f5685a400fceffcef91dfc1f7bbe2cc1c9d2a9c2
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2024-01-31 21:36:56 +0000

    Transition kernel_api over to new paging api

--- kernel_hal/src/paging2/mod.rs
+++ kernel_hal/src/paging2/mod.rs
@@ -1,12 +1,13 @@
 use core::fmt::Debug;
 use kernel_api::bridge::paging::MapPageError;
 use kernel_api::memory::{Frame, Page, PhysicalAddress, VirtualAddress};
+use crate::{Hal, HalTy};
 
-pub type KTableTy = <crate::HalTy as crate::Hal>::KTableTy;
-pub type TTableTy = <crate::HalTy as crate::Hal>::TTableTy;
+pub type KTableTy = <HalTy as crate::Hal>::KTableTy;
+pub type TTableTy = <HalTy as crate::Hal>::TTableTy;
 
 pub unsafe fn construct_tables() -> (KTableTy, TTableTy) {
-	<crate::HalTy as crate::Hal>::construct_tables()
+	<HalTy as crate::Hal>::construct_tables()
 }
 
 pub trait KTable: Debug {
@@ -31,4 +32,27 @@ pub trait TTable: KTable {
 	///
 	/// Figure out a better signature involving `Arc` or something
 	unsafe fn load(&self);
+}
+
+trait HackyExportTrick: KTable {
+	fn translate_page(&self, page: Page) -> Option<Frame>;
+	fn translate_address(&self, addr: VirtualAddress) -> Option<PhysicalAddress>;
+	fn map_page(&mut self, page: Page, frame: Frame) -> Result<(), MapPageError>;
+}
+
+impl HackyExportTrick for <HalTy as Hal>::KTableTy {
+	#[export_name = "__popcorn_paging_ktable_translate_page"]
+	fn translate_page(&self, page: Page) -> Option<Frame> {
+		<Self as KTable>::translate_page(self, page)
+	}
+
+	#[export_name = "__popcorn_paging_ktable_translate_address"]
+	fn translate_address(&self, addr: VirtualAddress) -> Option<PhysicalAddress> {
+		<Self as KTable>::translate_address(self, addr)
+	}
+
+	#[export_name = "__popcorn_paging_ktable_map_page"]
+	fn map_page(&mut self, page: Page, frame: Frame) -> Result<(), MapPageError> {
+		<Self as KTable>::map_page(self, page, frame)
+	}
 }
