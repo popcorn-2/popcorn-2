5b08dca078ff79afd83f258abbf4b1ec6c5b1f1e
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2024-02-17 11:48:58 +0000

    Add BitmapAllocator error reporting

--- bitmap_allocator/src/lib.rs
+++ bitmap_allocator/src/lib.rs
@@ -15,7 +15,14 @@ use core::ops::Range;
 use kernel_api::memory::{Frame, AllocError};
 use kernel_api::memory::allocator::{AllocationMeta, BackingAllocator, Config, SizedBackingAllocator, SpecificLocation};
 use kernel_api::sync::Mutex;
-use log::warn;
+use log::{debug, warn};
+
+macro_rules! alloc_err {
+    ($reason:literal) => {
+        debug!(concat!("BitmapAllocator: ", $reason));
+        return Err(AllocError.into());
+    };
+}
 
 #[derive(Debug, Eq, PartialEq)]
 enum FrameState {
@@ -87,14 +94,14 @@ impl BitmapAllocator {
             }
         }
 
-        return Err(AllocError);
+        alloc_err!("No free memory");
     }
 
     fn allocate_multiple_fast(&mut self, frame_count: usize) -> Result<Frame, AllocError> {
         assert!(frame_count > 1);
 
         // Cannot allocate bigger than number of bits in usize since can't check across boundaries
-        if frame_count > mem::size_of::<usize>() { return Err(AllocError); }
+        if frame_count > mem::size_of::<usize>() { alloc_err!("Too many pages for allocate_multiple_fast"); }
 
         // Create a mask of `frame_count` contiguous bits
         let mask = (2 << (frame_count - 1)) - 1;
@@ -131,7 +138,7 @@ impl BitmapAllocator {
             }
         }
 
-        Err(AllocError)
+        alloc_err!("No free memory");
     }
 
     #[cold]
@@ -164,7 +171,7 @@ impl BitmapAllocator {
             }
         }
 
-        if !found { Err(AllocError) }
+        if !found { alloc_err!("No free memory"); }
         else {
             let contiguous_frames_start = contiguous_frames_start.expect("unreachable");
             let start = self.first_frame + (contiguous_frames_start.0 * mem::size_of::<usize>()) + contiguous_frames_start.1;
@@ -225,7 +232,7 @@ unsafe impl BackingAllocator for Wrapped {
                     Ok(state) => state == FrameState::Free,
                     Err(_) => false,
                 });
-                if !free { return Err(AllocError); }
+                if !free { alloc_err!("Requested memory already allocated"); }
                 (addr..end).for_each(|f| guard.set_frame(f, FrameState::Allocated).expect("Must be in range"));
                 Ok(addr)
             }
