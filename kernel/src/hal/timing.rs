cc9455e7904a27829412dda6331019791a23eb9c
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2024-03-03 10:42:56 +0000

    Expand Timer interface

--- kernel/src/hal/timing.rs
+++ kernel/src/hal/timing.rs
@@ -3,8 +3,12 @@ use core::time::Duration;
 
 pub trait Timer {
 	fn get() -> Self;
-	fn set_irq_number(&mut self, irq: usize) -> Result<(), impl Debug + Display>;
-	fn get_time_periods(&self) -> impl IntoIterator<Item = Duration>;
-	fn set_time_period(&mut self, period: Duration) -> Result<(), impl Debug + Display>;
-	fn set_oneshot_time(&mut self) -> Result<(), impl Debug + Display>;
+	fn set_irq_number(&mut self, irq: usize) -> Result<(), impl Debug>;
+	fn get_time_periods(&self) -> Result<Duration, impl Debug>;
+	fn get_divisors(&self) -> impl IntoIterator<Item = u64>;
+	fn set_divisor(&mut self, divisor: u64) -> Result<(), impl Debug>;
+	fn set_oneshot_time(&mut self, ticks: u128) -> Result<(), impl Debug>;
+	fn start_periodic(&mut self, ticks: u128) -> Result<(), impl Debug>;
+	fn stop_periodic(&mut self);
+	fn eoi(&mut self);
 }
