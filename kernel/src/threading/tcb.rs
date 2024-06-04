2d8962f3097ded215a895a51005db6422fd1b86e
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2024-06-04 20:57:06 +0100

    Update ThreadControlBlock to store some state inside UnsafeCells, and expose two 'view types' for each view

--- kernel/src/threading/tcb.rs
+++ kernel/src/threading/tcb.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)] use crate::prelude::*;
 use alloc::borrow::Cow;
+use core::cell::UnsafeCell;
 use core::mem::MaybeUninit;
 use core::num::NonZeroUsize;
 use kernel_api::memory::mapping;
@@ -8,13 +9,81 @@ use kernel_api::memory::r#virtual::Global;
 use crate::hal::{Hal, HalTy, SaveState};
 use crate::hal::paging2::TTableTy;
 
-#[derive(Debug)]
-pub struct ThreadControlBlock {
-	pub ttable: TTableTy,
-	pub save_state: <HalTy as Hal>::SaveState,
-	pub name: Cow<'static, str>,
-	pub kernel_stack: Stack<'static, Global>,
-	pub state: ThreadState,
+macro_rules! gen_field {
+    /* mutable - thread */ (ref Thread, Thread, $field_ty:ty) => { &'a mut $field_ty };
+    /* mutable - pointer */ (ref Pointer, Pointer, $field_ty:ty) => { &'a mut $field_ty };
+	/* invisible */ (ref $_: ident, $_1: ident, $field_ty:ty) => { () };
+	/* immutable */ (ref $_: ident, $field_ty:ty) => { &'a $field_ty };
+    /* mutable - underlying */ ($_: ident, $field_ty:ty) => { ::core::cell::UnsafeCell<$field_ty> };
+	/* immutable - underlying */ ($field_ty:ty) => { $field_ty };
+}
+
+macro_rules! extract_field {
+    /* mutable */ ($tcb:ident, ref Thread, Thread, $field_name: ident) => { unsafe { &mut *$tcb.$field_name.get() } };
+    /* mutable */ ($tcb:ident, ref Pointer, Pointer, $field_name: ident) => { unsafe { &mut *$tcb.$field_name.get() } };
+	/* invisible */ ($tcb:ident, ref $_: ident, $_1: ident, $field_name: ident) => { () };
+	/* immutable */ ($tcb:ident, ref $_: ident, $field_name: ident) => { &$tcb.$field_name };
+}
+
+macro_rules! tcb_views {
+    (pub struct ThreadControlBlock {
+	    $($(#[mut = $field_place: ident])? $field_name: ident : $field_ty:ty),* $(,)?
+    }) => {
+		#[derive(Debug)]
+	    pub struct ThreadControlBlock {
+		    $(pub $field_name : gen_field!( $($field_place ,)? $field_ty)),*
+	    }
+
+		#[derive(Debug)]
+	    pub struct OwnedView<'a> {
+		    $(pub $field_name : gen_field!( ref Thread, $($field_place ,)? $field_ty)),*
+	    }
+
+		#[derive(Debug)]
+	    pub struct PointerView<'a> {
+		    $(pub $field_name : gen_field!( ref Pointer, $($field_place ,)? $field_ty)),*
+	    }
+
+	    impl ThreadControlBlock {
+		    fn new_inner($($field_name: $field_ty),*) -> ThreadControlBlock {
+			    ThreadControlBlock {
+				    $($field_name : ::core::convert::Into::into($field_name)),*
+			    }
+		    }
+	    }
+
+	    impl OwnedView<'_> {
+		    /// # Safety
+		    ///
+		    /// No other `OwnedView`s to the same [`ThreadControlBlock`] must exist at the same time
+		    unsafe fn from_tcb(tcb: &ThreadControlBlock) -> OwnedView<'_> {
+			    OwnedView {
+				    $($field_name : extract_field!( tcb, ref Thread, $($field_place ,)? $field_name)),*
+			    }
+		    }
+	    }
+
+	    impl PointerView<'_> {
+		    /// # Safety
+		    ///
+		    /// No other `PointerView`s to the same [`ThreadControlBlock`] must exist at the same time
+		    unsafe fn from_tcb(tcb: &ThreadControlBlock) -> PointerView<'_> {
+			    PointerView {
+				    $($field_name : extract_field!( tcb, ref Pointer, $($field_place ,)? $field_name)),*
+			    }
+		    }
+	    }
+    };
+}
+
+tcb_views! {
+	pub struct ThreadControlBlock {
+		ttable: TTableTy,
+		#[mut = Pointer] save_state: <HalTy as Hal>::SaveState,
+		name: Cow<'static, str>,
+		kernel_stack: Stack<'static, Global>,
+		state: ThreadState,
+	}
 }
 
 impl ThreadControlBlock {
@@ -24,15 +93,14 @@ impl ThreadControlBlock {
 			crate::paging_codes::THREAD_KERNEL_STACK,
 		).unwrap();
 
-		let mut new_thread = ThreadControlBlock {
+		let mut new_thread = ThreadControlBlock::new_inner(
 			ttable,
-			save_state: Default::default(),
+			Default::default(),
 			name,
-			kernel_stack: new_stack,
-			state: ThreadState::Ready,
-		};
-		let save_state = SaveState::new(&mut new_thread, startup, main, args.into_array());
-		new_thread.save_state = save_state;
+			new_stack,
+			ThreadState::Ready,
+		);
+		new_thread.save_state = UnsafeCell::new(SaveState::new(&mut new_thread, startup, main, args.into_array()));
 
 		new_thread
 	}
