55c89008591e50a898e877f89942d07c80542ddb
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-04-09 17:39:18 +0100

    Add `set_rsp0` function to TSS

--- kernel/src/hal/arch/amd64/tss.rs
+++ kernel/src/hal/arch/amd64/tss.rs
@@ -1,10 +1,11 @@
 #[allow(unused_imports)] use crate::prelude::*;
 use core::arch::asm;
 use core::cell::SyncUnsafeCell;
-use core::mem;
+use core::ptr::addr_of;
 use kernel_api::memory::VirtualAddress;
 use kernel_api::sync::OnceLock;
 
+// todo: make this thread_local
 pub static TSS: OnceLock<Tss> = OnceLock::new();
 
 struct StaticStack([u8; 3*4096]);
@@ -14,7 +15,8 @@ static DOUBLE_FAULT_STACK: SyncUnsafeCell<StaticStack> = SyncUnsafeCell::new(Sta
 #[repr(C, packed(4))]
 pub struct Tss {
     _res0: u32,
-    privilege_stack_table: [VirtualAddress; 3],
+    // use SyncUnsafeCell so we can modify through a shared reference
+    privilege_stack_table: [SyncUnsafeCell<VirtualAddress>; 3],
     _res1: u64,
     interrupt_stack_table: [VirtualAddress; 7],
     _res2: u64,
@@ -26,7 +28,7 @@ impl Tss {
     pub fn new() -> Tss {
         Tss {
             _res0: 0,
-            privilege_stack_table: [VirtualAddress::new(0); 3],
+            privilege_stack_table: [const { SyncUnsafeCell::new(VirtualAddress::new(0)) }; 3],
             _res1: 0,
             interrupt_stack_table: [
                 VirtualAddress::from(unsafe { DOUBLE_FAULT_STACK.get().add(1) }),
@@ -39,7 +41,13 @@ impl Tss {
             ],
             _res2: 0,
             _res3: 0,
-            io_map_base: mem::size_of::<Tss>() as u16,
+            io_map_base: size_of::<Tss>() as u16,
+        }
+    }
+    
+    pub extern "C" fn set_rsp0(&self, addr: VirtualAddress) {
+        unsafe {
+            asm!("lock xchg qword ptr [{}], {}", in(reg) addr_of!(self.privilege_stack_table[0]), inout(reg) addr.addr => _);
         }
     }
 
