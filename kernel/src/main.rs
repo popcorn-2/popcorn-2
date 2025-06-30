186f22351450202fd5a51fa1193a99be72d737f2
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-06-30 04:15:06 +0100

    Set up stack properly for popcorn abi

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -556,23 +556,88 @@ fn kmain(handoff_data: HandoffWrapper) -> ! {
 					.virtual_location(Location::At(Page::new(VirtualAddress::new(0x40000000))))
 					.protection(Protection::RWXU);
 
-			let stack = new_stack_in(config, u16::MAX).unwrap();
-
-			let ptr = stack.virtual_end().start().as_ptr().cast::<u64>();
+			let mut stack = new_stack_in(config, u16::MAX).unwrap();
 			
-			address_space.add_mapping("[stack@3]", stack);
-			
-			unsafe {
-				core::arch::asm!("stac");
-				ptr.offset(-1).write(0); // argc
-				ptr.offset(-2).write(0); // argv terminator
-				ptr.offset(-3).write(0); // env terminator
-				ptr.offset(-4).write(0); // once more for 16 byte alignment
-				core::arch::asm!("clac");
+			fn set_up_stack<'arg, 'env, 'handle>(
+				stack: &mut Stack<Userspace>,
+				arg: impl IntoIterator<Item = &'arg str>,
+				env: impl IntoIterator<Item = &'env str>,
+				handles: impl IntoIterator<IntoIter = impl Clone + Iterator<Item = (&'handle str, isize)>>
+			) -> VirtualAddress {
+				let (arg, env, handles) = (arg.into_iter(), env.into_iter(), handles.into_iter());
+				let mut stack_ptr = stack.virtual_end().as_ptr();
 
-				// address_space.add_mapping("[stack]", stack);
-				VirtualAddress::from(ptr.offset(-4))
+				fn write_strings<'a>(stack_ptr: &mut *mut u8, strings: impl Iterator<Item = &'a str>) -> Vec<usize> {
+					let mut ptrs = Vec::with_capacity(strings.size_hint().0);
+					for str in strings {
+						// write null terminator
+						*stack_ptr = unsafe { stack_ptr.offset(-1) };
+						unsafe { stack_ptr.write(0) };
+
+						// write string content
+						*stack_ptr = unsafe { stack_ptr.sub(str.len()) };
+						unsafe { ptr::copy_nonoverlapping(str.as_bytes().as_ptr(), *stack_ptr, str.len()); }
+
+						debug!("{:#p} = {str}", *stack_ptr);
+
+						// store start ptr
+						ptrs.push(stack_ptr.addr());
+					}
+					ptrs
+				}
+
+				let (handle_ids, handle_nums) = (handles.clone().map(|(s, _)| s), handles.map(|(_, i)| i as usize));
+				let arg_ptrs = write_strings(&mut stack_ptr, arg);
+				let env_ptrs = write_strings(&mut stack_ptr, env);
+				let handle_ptrs = write_strings(&mut stack_ptr, handle_ids);
+
+				let align_offset = stack_ptr.align_offset(size_of::<usize>());
+				let mut stack_ptr = unsafe { stack_ptr.cast::<usize>().byte_sub(size_of::<usize>() - align_offset) };
+
+				let total_count = 1 // argc
+						+ arg_ptrs.len()
+						+ 1 // argv terminator
+						+ env_ptrs.len()
+						+ 1 // envp terminator
+						+ 2 // auxv terminator
+						+ 2 * handle_ptrs.len() // handle map
+						+ 1; // handle terminator
+				stack_ptr = unsafe { stack_ptr.sub(total_count) };
+				if !stack_ptr.is_aligned_to(16) { stack_ptr = unsafe { stack_ptr.sub(1) }; }
+
+				for (offset, ptr) in core::iter::once(arg_ptrs.len()) // argc
+								.chain(arg_ptrs.into_iter()) // argv
+								.chain(core::iter::once(0)) // argv terminator
+								.chain(env_ptrs.into_iter()) // env
+								.chain(core::iter::once(0)) // env terminator
+								.chain(core::iter::repeat_n(0, 2)) // auxv terminator
+								.chain(handle_ptrs.into_iter().interleave_shortest(handle_nums)) // handle list
+								.chain(core::iter::once(0)) // handle list terminator
+						        .enumerate()
+				{
+					unsafe { stack_ptr.add(offset).write(ptr) };
+					debug!("{:#p} = {ptr:#x}", unsafe { stack_ptr.add(offset) });
+				}
+
+				assert!(stack_ptr.is_aligned_to(16), "userspace stack pointer not 16-byte aligned");
+
+				VirtualAddress::from(stack_ptr)
 			}
+
+			let addr = set_up_stack(
+				&mut stack,
+				["init", "hello", "world"],
+				["LANG=en_GB.UTF-8"],
+				[
+					("io.stdin", 0),
+					("io.stdout", 1),
+					("io.stderr", 2),
+				],
+			);
+
+			address_space.add_mapping("[stack@3]", stack);
+
+			addr
 		};
 
 		let file = elf::File::try_new(&init_data).unwrap();
