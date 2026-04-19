f052d2619ebe90daa24da2ca59c2f0f2077ac458
egkoppel <egkoppel@eliyahu.co.uk>
2026-04-19 23:55:03 +0100

    Revert "Add more thread APIs"

--- kernel/src/ipc/server/proc.rs
+++ kernel/src/ipc/server/proc.rs
@@ -14,7 +14,6 @@ use kernel_api::executor::block_on;
 use kernel_api::mapping::{Caching, Config, Mmap, Stack, Ty, UnsafeMmap};
 use kernel_api::memory::{PAGE_SIZE, PhysicalAddress, VirtualAddress, RawPage};
 use kernel_api::sync::{OnceLock, Spinlock};
-use kernel_api::syscall;
 use kernel_api::syscall::AsyncMap;
 use crate::ipc::{Error, protocol, server};
 use crate::ipc::ctor::{CtorContext, ProtocolVisitor};
@@ -38,19 +37,11 @@ pub struct ProcServer {
 enum Thread {
 	Building {
 		meta: Arc<ThreadMeta>,
-		startup: oneshot::Sender<StartupPacket>,
+		startup: oneshot::Sender<(VirtualAddress, VirtualAddress)>,
 		handle_nums: BTreeMap<Box<str>, u32>,
-		entry: Option<VirtualAddress>,
-		env_vars: Vec<Box<str>>,
-		args: Vec<Box<str>>,
+		entry: VirtualAddress,
 	},
 	Running(Arc<ThreadMeta>),
-}
-
-#[derive(Debug)]
-struct StartupPacket {
-	stack_top: VirtualAddress,
-	entry_override: Option<VirtualAddress>,
 }
 
 impl ProcServer {
@@ -185,32 +176,6 @@ impl protocol::generated::core::proc::Thread for ProcServer {
 		core::future::ready(res)
 	}
 
-	async fn exit(&self, handle: isize, code: isize) -> Result<(), Error> {
-		unsupported_other_thread(handle)?;
-
-		threading::exit(code)
-	}
-
-	async fn join(&self, handle: isize) -> Result<isize, Error> {
-		let thread = {
-			let guard = self.threads.lock();
-			let thread = match guard.get(handle as usize).ok_or(Error::InvalidHandle)? {
-				Thread::Building { meta, .. } => meta,
-				Thread::Running(meta) => meta,
-			};
-
-			// todo: check if already a thread waiting on the process
-
-			Arc::clone(thread)
-		};
-
-		let res = thread.join().await;
-
-		let _ = self.threads.lock().try_remove(handle as usize);
-
-		Ok(res)
-	}
-
 	async fn yield_now(&self, handle: isize) -> Result<(), Error> {
 		unsupported_other_thread(handle)?;
 
@@ -295,9 +260,7 @@ impl protocol::generated::core::proc::Builder for ProcServer {
 			meta,
 			startup,
 			mut handle_nums,
-			env_vars,
-			args,
-			entry,
+			entry
 		} = (unsafe { ptr::read(thread) }) else { unsafe { unreachable_unchecked(); } };
 		
 		// fixme: hacky
@@ -312,12 +275,12 @@ impl protocol::generated::core::proc::Builder for ProcServer {
 		handle_nums.insert(Box::from("thread.main"), main_thread_handle);
 
 		let stack_top = {
-			let (_, mut stack) = Config::new(NonZero::new(128).unwrap(), Ty::USER_STACK)
+			let (_, mut stack) = Config::new(NonZero::new(64).unwrap(), Ty::USER_STACK)
 					.protection(true, false, true)
 					.virtual_location(RawPage::new(0x40000000))
 					.map_in::<Stack>(format!("[stack:{main_thread_handle}]").into(), &meta.address_space)?;
 
-			crate::loader::set_up_stack(&mut stack, args, env_vars, handle_nums)
+			crate::loader::set_up_stack(&mut stack, [], [], handle_nums)
 		};
 
 		unsafe {
@@ -327,7 +290,8 @@ impl protocol::generated::core::proc::Builder for ProcServer {
 			);
 		}
 
-		startup.send(StartupPacket { stack_top, entry_override: entry }).unwrap();
+		startup.send((entry, stack_top)).unwrap();
+
 		
 		Ok(ReturnHandle::New(
 			handle,
@@ -362,273 +326,63 @@ impl protocol::generated::core::proc::Builder for ProcServer {
 		Ok(())
 	}
 
-	async fn add_env_var(&self, handle: isize, val: &str) -> Result<(), Error> {
-		let mut guard = self.threads.lock();
-		debug!("add env_var to {handle:#x} ({:#x?})", guard.get_mut(handle as usize));
-		let Some(Thread::Building { env_vars, .. }) = guard.get_mut(handle as usize) else {
-			return Err(Error::UnsupportedProtocol);
-		};
-
-		env_vars.push(Box::from(val));
-
-		Ok(())
-	}
-
-	async fn add_arg(&self, handle: isize, val: &str) -> Result<(), Error> {
-		let mut guard = self.threads.lock();
-		debug!("add arg to {handle:#x} ({:#x?})", guard.get_mut(handle as usize));
-		let Some(Thread::Building { args, .. }) = guard.get_mut(handle as usize) else {
-			return Err(Error::UnsupportedProtocol);
-		};
-
-		args.push(Box::from(val));
-
-		Ok(())
-	}
-
 	async fn new_from(&self, endpoint: &str, handle: Arc<Handle>) -> Result<ReturnHandle, Error> {
 		info!("spawn process `{endpoint}` with handle {handle:#x?}");
 
-		async fn read_elf_header_from(handle: &Arc<Handle>) -> syscall::Result<FileHeader> {
-			if !handle.has_protocols(&[<dyn Read>::UID, <dyn Seek>::UID]) { return Err(Error::InvalidArg); }
-
-			let mut raw_header = MaybeUninit::<FileHeaderRaw>::uninit();
-
-			let res = handle.kernel_syscall(
-				<dyn Read>::UID,
-				1,
-				[
-					raw_header.as_mut_ptr().addr(),
-					size_of::<FileHeaderRaw>(),
-					0,
-					0,
-				]
-			).await?;
-
-			debug!("read {res} bytes from ELF file");
-			if (res as usize) < size_of::<FileHeaderRaw>() { return Err(Error::EndOfData); }
-			let header = <&FileHeader>::try_from(unsafe { raw_header.assume_init_ref() });
-			info!("elf header: {header:#?}");
-			let header: &FileHeader = header.map_err(|e| {
-				debug!("failed to parse ELF header: {e:?}");
-				Error::InvalidArg
-			})?;
-			if header.width != Width::_64 {
-				debug!("not 64 bit");
-				return Err(Error::InvalidArg);
-			}
-			if header.endianness != Endianness::Little {
-				debug!("not little endian");
-				return Err(Error::InvalidArg);
-			}
-			if header.isa != Isa::Amd64 {
-				debug!("not amd64");
-				return Err(Error::InvalidArg);
-			}
-			if header.elf_version != 1 {
-				debug!("not elf v1");
-				return Err(Error::InvalidArg);
-			}
-
-			// move out of a MaybeUninit which we don't use again
-			Ok(unsafe { ptr::read(header) })
-		}
-
-		async fn load_elf_from_with_offset(handle: &Arc<Handle>, header: &FileHeader, address_space: &AddressSpace, base: Option<VirtualAddress>) -> syscall::Result<VirtualAddress> {
-			let offset = base.map(|addr| addr.addr).unwrap_or(0);
-
-			let Range {
-				start: mut program_header_start,
-				end: program_header_end,
-			} = header.program_header();
-
-			let program_header_count = (program_header_end - program_header_start) / size_of::<ProgramHeaderEntry64>();
-
-			debug!("program header from {program_header_start}->{program_header_end} ({program_header_count})");
-
-			let mut interp_handle = None;
-			let mut highest_addr = VirtualAddress::new(0);
-
-			for _ in 0..program_header_count {
-				let _ = block_on(
-					handle.kernel_syscall(
-						<dyn Seek>::UID,
-						2,
-						[
-							program_header_start,
-							0,
-							0,
-							0,
-						]
-					)
-				)?;
-
-				let mut raw = MaybeUninit::<ProgramHeaderEntry64>::uninit();
-				let res = block_on(
-					handle.kernel_syscall(
-						<dyn Read>::UID,
-						1,
-						[
-							raw.as_mut_ptr().addr(),
-							size_of::<ProgramHeaderEntry64>(),
-							0,
-							0,
-						]
-					)
-				)?;
-				if (res as usize) < size_of::<ProgramHeaderEntry64>() { Err(Error::EndOfData)?; }
-				let segment = unsafe { raw.assume_init() };
-				info!("found header {segment:#?}");
-
-				program_header_start += size_of::<ProgramHeaderEntry64>();
-
-				if segment.segment_type == SegmentType::LOAD {
-					let _ = block_on(
-						handle.kernel_syscall(
-							<dyn Seek>::UID,
-							2,
-							[
-								segment.file_location().0.start,
-								0,
-								0,
-								0,
-							]
-						)
-					)?;
-
-					assert_eq!(segment.alignment as usize, PAGE_SIZE, "Not designed for !=1 page alignment");
-
-					let addr = VirtualAddress::new(segment.vaddr.try_into().unwrap()) + offset;
-					let segment_page_offset = addr - *addr.align_down_to_page();
-
-					let len = segment_page_offset + usize::try_from(segment.memory_size).unwrap();
-					if let Some(len) = NonZero::new(len.div_ceil(PAGE_SIZE)) {
-						let (mapping_key, page) = {
-							let (mapping_key, mapping) = Config::new(len, Ty::USER_CODE)
-									.protection(
-										true, // segment.segment_flags.contains(SegmentFlags::Writeable),
-										segment.segment_flags.contains(SegmentFlags::Executable),
-										true
-									)
-									.virtual_location(addr.align_down_to_page())
-									.map_in::<UnsafeMmap>(Cow::Owned(String::from(&**handle.endpoint())), &address_space)?;
-
-							assert!(segment.file_size <= segment.memory_size);
-
-							if mapping.as_ptr_range().end.addr() > highest_addr {
-								highest_addr = mapping.as_ptr_range().end.addr();
-							}
-
-							(mapping_key, mapping.virtual_valid_start())  // fixme: is it sound to drop the mapping here?
-						};
-
-						let res = match block_on(
-							handle.kernel_syscall(
-								<dyn Read>::UID,
-								1,
-								[
-									page.as_ptr().addr() + segment_page_offset,
-									segment.file_size.try_into().unwrap(),
-									0,
-									0,
-								]
-							)
-						) {
-							Ok(res) => res,
-							e @ Err(Error::InvalidArg | Error::InvalidHandle | Error::InvalidPointer) => e.expect("args should be valid"),
-							Err(e) => Err(e)?,
-						};
-						if (res as u64) < segment.file_size { Err(Error::EndOfData)?; }
-
-						let mut mapping = address_space.get(mapping_key).expect("mapping should still exist");
-						let zero_start = unsafe {
-							let ptr = mapping.as_mut_ptr()
-							                 .byte_add(segment.file_size.try_into().unwrap())
-							                 .byte_add(segment_page_offset);
-							kernel_api::ptr::slice_from_raw_parts_mut(
-								ptr,
-								(segment.memory_size - segment.file_size).try_into().unwrap(),
-							)
-						};
-						zero_start.fill(0).unwrap();
-
-						// todo: make read-only pages actually read-only
-					}
-				} else if segment.segment_type == SegmentType::INTERPRETER {
-					let _ = block_on(
-						handle.kernel_syscall(
-							<dyn Seek>::UID,
-							2,
-							[
-								segment.file_location().0.start,
-								0,
-								0,
-								0,
-							]
-						)
-					)?;
-
-					let mut buffer = Vec::<u8>::with_capacity(segment.file_size as usize);
-
-					let res = match block_on(
-						handle.kernel_syscall(
-							<dyn Read>::UID,
-							1,
-							[
-								buffer.as_mut_ptr().addr(),
-								segment.file_size.try_into().unwrap(),
-								0,
-								0,
-							]
-						)
-					) {
-						Ok(res) => res,
-						e @ Err(Error::InvalidArg | Error::InvalidHandle | Error::InvalidPointer) => e.expect("args should be valid"),
-						Err(e) => Err(e)?,
-					};
-					if (res as u64) < segment.file_size { Err(Error::EndOfData)?; }
-
-					unsafe { buffer.set_len(segment.file_size as usize) };
-
-					// chop off the NUL byte
-					let interpreter_path = str::from_utf8(&buffer[..buffer.len()-1]).unwrap();
-					info!("interpreter path: {}", interpreter_path);
-
-					interp_handle = Some(block_on(crate::ipc::abi_v1::open_async(interpreter_path, &[<dyn Read>::UID, <dyn Seek>::UID]))?);
-				}
-			}
-
-			if let Some(interp_handle) = interp_handle {
-				let base = highest_addr.align_up_to_page() + 8usize;
-				info!("loading interpreter at {base:#x}");
-
-				let interp_header = block_on(read_elf_header_from(&interp_handle))?;
-
-				if interp_header.file_type != Type::Shared {
-					warn!("not shared library");
-					return Err(Error::InvalidArg);
-				}
-
-				Box::pin(load_elf_from_with_offset(
-					&interp_handle,
-					&interp_header,
-					address_space,
-					Some(*base),
-				)).await
-			} else {
-				Ok(VirtualAddress::new(header.entry_point()) + offset)
-			}
-		}
-
+		if !handle.has_protocols(&[<dyn Read>::UID, <dyn Seek>::UID]) { return Err(Error::InvalidArg); }
 		let address_space = AddressSpace::empty()?;
 
-		let header = read_elf_header_from(&handle).await?;
+		let mut raw_header = MaybeUninit::<FileHeaderRaw>::uninit();
+
+		let res = handle.kernel_syscall(
+			<dyn Read>::UID,
+			1,
+			[
+				raw_header.as_mut_ptr().addr(),
+				size_of::<FileHeaderRaw>(),
+				0,
+				0,
+			]
+		).await?;
+
+		debug!("read {res} bytes from ELF file");
+		if (res as usize) < size_of::<FileHeaderRaw>() { return Err(Error::EoF); }
+		let header = <&FileHeader>::try_from(unsafe { raw_header.assume_init_ref() });
+		debug!("elf header: {header:#?}");
+		let header: &FileHeader = header.map_err(|e| {
+			debug!("failed to parse ELF header: {e:?}");
+			Error::InvalidArg
+		})?;
+		if header.width != Width::_64 {
+			debug!("not 64 bit");
+			return Err(Error::InvalidArg);
+		}
+		if header.endianness != Endianness::Little {
+			debug!("not little endian");
+			return Err(Error::InvalidArg);
+		}
 		if header.file_type != Type::Executable {
-			warn!("not executable");
+			debug!("not executable");
+			return Err(Error::InvalidArg);
+		}
+		if header.isa != Isa::Amd64 {
+			debug!("not amd64");
+			return Err(Error::InvalidArg);
+		}
+		if header.elf_version != 1 {
+			debug!("not elf v1");
 			return Err(Error::InvalidArg);
 		}
 
-		let (send_startup, receive_startup) = oneshot::channel::<StartupPacket>();
+		let Range {
+			start: mut program_header_start,
+			end: program_header_end,
+		} = header.program_header();
+		let program_header_count = (program_header_end - program_header_start) / size_of::<ProgramHeaderEntry64>();
+
+		debug!("program header from {program_header_start}->{program_header_end} ({program_header_count})");
+
+		let (send_startup, receive_startup) = oneshot::channel::<(VirtualAddress, VirtualAddress)>();
 		let (send_setup, receive_setup) = oneshot::channel();
 
 		let tid = {
@@ -645,26 +399,118 @@ impl protocol::generated::core::proc::Builder for ProcServer {
 				HandleMap::new(),
 				Arc::new(AsyncMap::new()),
 				move || {
-					let entrypoint = block_on(load_elf_from_with_offset(
-						&handle,
-						&header,
-						&address_space,
-						None,
-					));
+					let result = try {
+						for _ in 0..program_header_count {
+							let _ = block_on(
+								handle.kernel_syscall(
+									<dyn Seek>::UID,
+									2,
+									[
+										program_header_start,
+										0,
+										0,
+										0,
+									]
+								)
+							)?;
 
-					debug!("program load done");
+							let mut raw = MaybeUninit::<ProgramHeaderEntry64>::uninit();
+							let res = block_on(
+								handle.kernel_syscall(
+									<dyn Read>::UID,
+									1,
+									[
+										raw.as_mut_ptr().addr(),
+										size_of::<ProgramHeaderEntry64>(),
+										0,
+										0,
+									]
+								)
+							)?;
+							if (res as usize) < size_of::<ProgramHeaderEntry64>() { Err(Error::EoF)?; }
+							let segment = unsafe { raw.assume_init() };
+							info!("found header {segment:#?}");
 
-					send_setup.send(entrypoint.map(|_| ())).unwrap();
+							program_header_start += size_of::<ProgramHeaderEntry64>();
 
-					if entrypoint.is_err() { return; }
+							if segment.segment_type == SegmentType::LOAD {
+								let _ = block_on(
+									handle.kernel_syscall(
+										<dyn Seek>::UID,
+										2,
+										[
+											segment.file_location().0.start,
+											0,
+											0,
+											0,
+										]
+									)
+								)?;
 
-					let startup = block_on(receive_startup).unwrap();
-					let entrypoint = match startup.entry_override {
-						Some(entrypoint) => entrypoint,
-						None => entrypoint.expect("should not have received startup packet if loading errored"),
+								assert_eq!(segment.alignment, 4096, "Not designed for !=1 page alignment");
+
+								let addr = VirtualAddress::new(segment.vaddr.try_into().unwrap());
+								let segment_page_offset = addr - *addr.align_down_to_page();
+
+								let len = segment_page_offset + usize::try_from(segment.memory_size).unwrap();
+								if let Some(len) = NonZero::new(len.div_ceil(4096)) {
+									let (mapping_key, page) = {
+										let (mapping_key, mapping) = Config::new(len, Ty::USER_CODE)
+												.protection(
+													true, // segment.segment_flags.contains(SegmentFlags::Writeable),
+													segment.segment_flags.contains(SegmentFlags::Executable),
+													true
+												)
+												.virtual_location(addr.align_down_to_page())
+												.map_in::<UnsafeMmap>("".into(), &address_space)
+												.map_err(From::from)?;
+
+										assert!(segment.file_size <= segment.memory_size);
+
+										(mapping_key, mapping.virtual_valid_start())  // fixme: is it sound to drop the mapping here?
+									};
+
+									let res = match block_on(
+										handle.kernel_syscall(
+											<dyn Read>::UID,
+											1,
+											[
+												page.as_ptr().addr() + segment_page_offset,
+												segment.file_size.try_into().unwrap(),
+												0,
+												0,
+											]
+										)
+									) {
+										Ok(res) => res,
+										e @ Err(Error::InvalidArg | Error::InvalidHandle | Error::InvalidPointer) => e.expect("args should be valid"),
+										Err(e) => Err(e)?,
+									};
+									if (res as u64) < segment.file_size { Err(Error::EoF)?; }
+
+									let mut mapping = address_space.get(mapping_key).expect("mapping should still exist");
+									let zero_start = unsafe {
+										let ptr = mapping.as_mut_ptr()
+										                 .byte_add(segment.file_size.try_into().unwrap())
+										                 .byte_add(segment_page_offset);
+										kernel_api::ptr::slice_from_raw_parts_mut(
+											ptr,
+											(segment.memory_size - segment.file_size).try_into().unwrap(),
+										)
+									};
+									zero_start.fill(0).unwrap();
+
+									// todo: make read-only pages actually read-only
+								}
+							}
+						}
 					};
 
-					hal::switch_to_userspace_at(entrypoint, startup.stack_top);
+					debug!("program load done");
+					send_setup.send(result).unwrap();
+
+					let startup = block_on(receive_startup).unwrap();
+					hal::switch_to_userspace_at(startup.0, startup.1);
 				}
 			)?;
 
@@ -672,9 +518,7 @@ impl protocol::generated::core::proc::Builder for ProcServer {
 				meta,
 				startup: send_startup,
 				handle_nums: BTreeMap::new(),
-				env_vars: vec![],
-				args: vec![],
-				entry: None,
+				entry: VirtualAddress::new(header.entry_point()),
 			});
 
 			tid
--- kernel_api/src/syscall/result.rs
+++ kernel_api/src/syscall/result.rs
@@ -43,8 +43,7 @@ define_error! {
 		InvalidArg = 13,
 		AsyncUnsupported = 14,
 		FutureCompat = 15,
-		// Object is not long enough for passed arguments
-		EndOfData = 16,
+		EoF = 16,
 		ProtocolOverlap = 17,
 	}
 }
