fcbd3ebe371afc0157d4a7a0bb651b0070e90daf
Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>
2026-08-10 21:33:57 +0100

    move asan conversion funcs out of cfg(full)

--- kernel_api/src/memory/asan.rs
+++ kernel_api/src/memory/asan.rs
@@ -45,6 +45,21 @@ const SHADOW_MAP_SIZE: usize = cfg_select! {
 	target_arch = "x86_64" => 16*1024*1024*1024*1024,
 };
 
+/// Converts the passed `address` into the corresponding address in the shadow map.
+#[must_use]
+pub fn mem_to_shadow(address: VirtualAddress) -> VirtualAddress {
+	let ret = (address.addr >> 3) + SHADOW_MAP_SHIFT;
+	let ret = VirtualAddress::new(ret);
+	debug_assert!(ret >= SHADOW_MAP_START && ret < SHADOW_MAP_END, "address = {address:#x}, ret = {ret:#x}, start = {SHADOW_MAP_START:#x}, end = {SHADOW_MAP_END:#x}");
+	ret
+}
+
+/// Converts the number of bytes into the lower bound number of bytes in the shadow map.
+#[must_use]
+pub const fn count_to_shadow(count: usize) -> usize {
+	count / 8
+}
+
 #[cfg(feature = "full")] pub use full::*;
 #[cfg(feature = "full")] mod full {
 	pub use super::*;
@@ -505,18 +520,5 @@ Shadow byte legend (one shadow byte represents 8 kernel bytes):
 			7 => write_shadow_map_for(last, 7),
 			_ => unreachable!("x % 8 < 8"),
 		}
-	}
-
-	/// Converts the passed `address` into the corresponding address in the shadow map
-	pub fn mem_to_shadow(address: VirtualAddress) -> VirtualAddress {
-		let ret = (address.addr >> 3) + SHADOW_MAP_SHIFT;
-		let ret = VirtualAddress::new(ret);
-		debug_assert!(ret >= SHADOW_MAP_START && ret < SHADOW_MAP_END, "address = {address:#x}, ret = {ret:#x}, start = {SHADOW_MAP_START:#x}, end = {SHADOW_MAP_END:#x}");
-		ret
-	}
-
-	/// Converts the number of bytes into the lower bound number of bytes in the shadow map
-	pub fn count_to_shadow(count: usize) -> usize {
-		count / 8
 	}
 }
