02acb148150fc6e00bf8218969f7e08d870cb3eb
Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>
2026-08-10 21:46:54 +0100

    sort out lint feature flags

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -3,6 +3,8 @@
 #![feature(arbitrary_self_types)]
 #![feature(error_iter)]
 #![feature(duration_integer_division)]
+#![feature(strict_provenance_lints)]
+#![cfg_attr(doc, feature(rustdoc_missing_doc_code_examples))]
 #![no_main]
 #![no_std]
 
