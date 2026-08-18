ae262fd638a0976d07f8ec07b1eed4edfe59bb82
Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>
2026-08-18 16:12:35 +0100

    cleanup bootloader paging and move into arch submodule

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -18,9 +18,7 @@ use uefi::{entry, println, Status, boot};
 mod logging;
 mod elf;
 mod fs;
-
-const PAGE_MAP_OFFSET: u64 = 0xffff_8000_0000_0000;
-const PAGE_MAP_OFFSET_LEN: u64 = 2u64.pow(46);
+mod arch;
 
 #[entry]
 fn bootloader_entry() -> Status {
