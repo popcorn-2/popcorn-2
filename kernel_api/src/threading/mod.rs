752f103aa3e3c0800b0dee470dcea246ee3b8cf1
Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>
2026-09-13 21:44:30 +0100

    kernel_api: add TaskRef
    Add a TaskRef struct to kernel_api::threading which contains a tagged
    static reference to a task.
    
    Change-Id: I589913cd0e8698b155fd3cb00a5ad96391ea7e69
    Signed-off-by: Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>

--- kernel_api/src/threading/mod.rs
+++ kernel_api/src/threading/mod.rs
@@ -4,8 +4,10 @@ mod meta;
 mod state;
 
 use core::borrow::Borrow;
+use core::ptr::NonNull;
 pub use meta::*;
 pub use state::*;
+use crate::ptr::TaggedNonNull;
 
 /// The numerical ID of a thread
 /// 
@@ -29,5 +31,34 @@ impl ThreadId {
 impl Borrow<ThreadId> for ThreadMeta {
 	fn borrow(&self) -> &ThreadId {
 		&self.thread_id
+	}
+}
+
+/// A cheap to copy handle to a task.
+#[derive(Copy, Clone, Debug, Ord, PartialOrd, PartialEq, Eq, Hash)]
+pub struct TaskRef {
+	tagged_ref: TaggedNonNull<u8>,
+}
+
+// SAFETY: constructor only allows pointers to `Sync` types so `Send` references are sound
+unsafe impl Send for TaskRef {}
+// SAFETY: constructor only allows pointers to `Sync` types so `Sync` references are sound
+unsafe impl Sync for TaskRef {}
+
+impl TaskRef {
+	#[doc(hidden)]
+	pub fn new<T: Sync>(r: &'static T, tag: usize) -> Self {
+		Self {
+			tagged_ref: TaggedNonNull::new(
+				NonNull::from_ref(r).cast(),
+				tag,
+			),
+		}
+	}
+
+	pub fn as_ptr(self) -> NonNull<u8> { self.tagged_ref.as_ptr() }
+	pub fn generation(self) -> usize { self.tagged_ref.tag() }
+	pub fn addr_eq(self, other: TaskRef) -> bool {
+		TaggedNonNull::addr_eq(&self.tagged_ref, &other.tagged_ref)
 	}
 }
