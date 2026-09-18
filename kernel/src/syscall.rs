8df0e91862a2b8d978911f6c4f9c9b790a17e23b
Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>
2026-09-18 17:12:17 +0100

    kernel: flesh out syscall entry
    - Get current task from registry
    - Get requested handle from handle registry
    - Branch to syscall if handle references a koid
    
    Change-Id: I98d4c6769dcc86359a03146211dd11c3e1825579
    Signed-off-by: Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>

--- kernel/src/syscall.rs
+++ kernel/src/syscall.rs
@@ -1,6 +1,7 @@
-use kernel_api::dbg;
-use crate::arch;
 use kernel_api::syscall;
+use crate::{arch, ebr, percpu};
+use crate::task::TaskRefExt;
+use crate::task::Task;
 
 #[derive(Debug)]
 pub struct EntryParams {
@@ -25,5 +26,60 @@ pub struct ExitParams {
 #[inline(always)]
 pub unsafe fn entry(params: EntryParams) -> syscall::Result<ExitParams> {
 	debug!("enter syscall: {params:#x?}");
+	let caller = {
+		let task_ref = percpu!(current_task).get();
+		let task = task_ref.get();
+		// SAFETY: syscalls can only be made from a running thread
+		unsafe { task.unwrap_unchecked() }
+	};
+
+	let exit_params = {
+		let ebr = percpu!(epoch).pin();
+		let this = caller.handles().get(params.handle_num, &ebr)?;
+
+		let target = if this.is_system() {
+			return system_syscall(&ebr, this.koid(), caller, params);
+		} else {
+			this.target().get().ok_or(syscall::Error::DeadServer)?
+		};
+
+		todo!("check target is blocked waiting for syscall");
+
+		let oob_bytes = params.oob_args.iter().sum::<usize>();
+
+		let caller_trampoline = caller.syscall_trampoline_page();
+		let target_trampoline = target.syscall_trampoline_page();
+		unsafe {
+			core::arch::asm!(
+				"rep movsb",
+				in("rdi") target_trampoline,
+				in("rsi") caller_trampoline,
+				inout("rcx") oob_bytes => _,
+			)
+		}
+
+		params.stack_frame.store(caller);
+		percpu!(current_task).set(Some(this.target()));
+
+		ExitParams {
+			oid: this.oid(),
+			method: params.method,
+			interface: params.interface,
+			integer_args: params.integer_args,
+			oob_args: params.oob_args,
+			stack_frame: arch::SyscallStackFrame::from(target),
+		}
+	};
+
+	debug!("exit syscall: {exit_params:#x?}");
+	Ok(exit_params)
+}
+
+fn system_syscall(
+	ebr: &ebr::EpochGuard,
+	oid: usize,
+	caller: &Task,
+	params: EntryParams,
+) -> syscall::Result<ExitParams> {
 	todo!()
 }
