5896fd8d54c1eb674d1fb6d3dda44fe152d241ae
Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>
2026-09-19 18:15:54 +0100

    kernel: fix argv generation
    Existing implementation did not properly write a char** and instead just
     wrote the address of the first string to argv.
    
    Change-Id: Id2b6edbe64848ef273d98aeff642785179939115
    Signed-off-by: Eliyahu Gluschove-Koppel <egkoppel@eliyahu.co.uk>

--- kernel/src/task/startup.rs
+++ kernel/src/task/startup.rs
@@ -50,15 +50,17 @@ impl ProcInfo {
 		const {
 			assert!(align_of_val("") == 1, "strings must be 1 byte aligned");
 			assert!(align_of::<ProcInfo>() <= PAGE_SIZE, "proc info must be aligned to page size");
-			assert!(align_of::<ProcInfo>() >= align_of::<NamedHandle>(), "proc info must be aligned to page size");
+			assert!(align_of::<ProcInfo>() >= align_of::<NamedHandle>(), "NamedHandle must fit after proc info");
+			assert!(align_of::<NamedHandle>() >= align_of::<*const core::ffi::c_char>(), "argv array must fit after handles");
 		}
 
 		let proc_info_size = size_of::<ProcInfo>();
 		let handle_tab_len = size_of::<NamedHandle>() * (handles.len() + 1);
+		let argv_len = size_of::<*const core::ffi::c_char>() * (args.len() + 1);
 		let string_tab_len = args.iter().map(|str| str.len() + 1).sum::<usize>() +
 			handles.iter().map(|(str, _)| str.len() + 1).sum::<usize>();
 
-		let page_count = (proc_info_size + handle_tab_len + string_tab_len).div_ceil(PAGE_SIZE);
+		let page_count = (proc_info_size + handle_tab_len + argv_len + string_tab_len).div_ceil(PAGE_SIZE);
 		let mut mapping = Config::new(
 			NonZero::new(page_count).expect("proc info should be non-zero size"),
 			Ty::UNKNOWN,
@@ -67,14 +69,24 @@ impl ProcInfo {
 
 		let ptr = mapping.mapping.as_mut_ptr().cast::<ProcInfo>();
 		let named_handles = unsafe { ptr.add(1).cast::<NamedHandle>() };
-		let mut next_string = unsafe { named_handles.add(handles.len() + 1).cast::<u8>() };
+		let argv_array = unsafe { named_handles.add(handles.len() + 1).cast::<VirtualAddress>() };
+		let mut next_string = unsafe { argv_array.add(args.len() + 1).cast::<u8>() };
 
+		// add handle list terminator
 		unsafe {
 			named_handles.add(handles.len())
 				.write_other_address_space(NamedHandle::TERMINATOR)
 				.expect("allocated memory should be valid");
 		}
 
+		// add argv array terminator
+		unsafe {
+			argv_array.add(args.len())
+				.write_other_address_space(VirtualAddress::new(0))
+				.expect("allocated memory should be valid");
+		}
+
+		// write handle list
 		for (i, (name, handle)) in handles.into_iter().enumerate() {
 			let handle_num = task.handles.push(handle, ebr)?;
 			let handle = NamedHandle {
@@ -82,13 +94,16 @@ impl ProcInfo {
 				handle_num,
 			};
 			unsafe {
+				// write handle itself
 				named_handles.add(i)
 					.write_other_address_space(handle)
 					.expect("allocated memory should be valid");
+				// write handle name to strtab
 				next_string.copy_from_other_address_space(
 					name.as_ptr(),
 					name.len()
 				).expect("allocated memory should be valid");
+				// add null terminator to string
 				next_string.add(name.len())
 					.write_other_address_space(0)
 					.expect("allocated memory should be valid");
@@ -96,13 +111,18 @@ impl ProcInfo {
 			}
 		}
 
-		let argv = next_string.addr();
-		for arg in args.iter() {
+		for (i, arg) in args.iter().enumerate() {
 			unsafe {
+				// write pointer to string into argv array
+				argv_array.add(i)
+					.write_other_address_space(next_string.addr())
+					.expect("allocated memory should be valid");
+				// write arg itself to strtab
 				next_string.copy_from_other_address_space(
 					arg.as_ptr(),
 					arg.len()
 				).expect("allocated memory should be valid");
+				// add null terminator to string
 				next_string.add(arg.len())
 					.write_other_address_space(0)
 					.expect("allocated memory should be valid");
@@ -112,7 +132,7 @@ impl ProcInfo {
 
 		ptr.write_other_address_space(ProcInfo::new_raw(
 			args.len().try_into().map_err(|_| syscall::Error::Overflow)?,
-			argv,
+			argv_array.addr(),
 			named_handles.addr(),
 			info_ty,
 			info_ptr,
